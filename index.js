7b7173706e6da5d6d40e969263c260b04286575b
Mariusmivw <mariusmivw@gmail.com>
2019-12-18 22:41:05 +0100

    indent using tabs

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const { parse } = require("url");
-const { readFileSync } = require("fs");
+const { parse } = require('url');
+const { readFileSync } = require('fs');
 
 function serve({ path, file, headers }, server) {
-  const evs = server.listeners("request").slice(0);
-  server.removeAllListeners("request");
-  server.on("request", (req, res) => {
-    if (parse(req.url).pathname === path) {
-      for (const header in headers) {
-        res.setHeader(header, headers[header]);
-        res.write(readFileSync(file));
-        res.end();
-      }
-    } else {
-      for (const ev of evs) {
-        ev.call(server, req, res);
-      }
-    }
-  });
+	const evs = server.listeners('request').slice(0);
+	server.removeAllListeners('request');
+	server.on('request', (req, res) => {
+		if (parse(req.url).pathname === path) {
+			for (const header in headers) {
+				res.setHeader(header, headers[header]);
+				res.write(readFileSync(file));
+				res.end();
+			}
+		} else {
+			for (const ev of evs) {
+				ev.call(server, req, res);
+			}
+		}
+	});
 }
 serve.serve = serve;
 
